2216074db737adb7d0516e118c5d3d6f83ebfbfc
Ahmed EL Abid <ahmedelabide@gmail.com>
2024-07-29 12:26:24 -0700

    mandatory

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,38 +1,32 @@
 #!/usr/bin/python3
-"A Python script that saves data to a JSON file."
+"""Produces a JSON dump from mock user data using the JSON Placeholder API."""
 import json
 import requests
-import sys
-
-
-def tasks_done(id):
-    '''Script that exports an employee's TODO tasks to a JSON file.
-Parameters:
-employee_id: An integer representing the ID of the employee.'''
-
-    url = "https://jsonplaceholder.typicode.com/users/{}".format(id)
-    response = requests.get(url)
-    response_json = response.json()
-    employee_name = response_json.get("name")
-
-    url = "https://jsonplaceholder.typicode.com/users/{}/todos".format(id)
-    todos = requests.get(url)
-    todos_json = todos.json()
-    task_list = []
-
-    for task in todos_json:
-        task_dict = {}
-        task_dict["task"] = task.get("title")
-        task_dict["completed"] = task.get("completed")
-        task_dict["username"] = employee_name
-        task_list.append(task_dict)
-
-    todos = {"{}".format(id): task_list}
-
-    file_name = "{}.json".format(id)
-    with open(file_name, "a") as fd:
-        json.dump(todos, fd)
+from sys import argv
 
 
 if __name__ == "__main__":
-    tasks_done(sys.argv[1])
+    user_id = argv[1]
+    api = 'https://jsonplaceholder.typicode.com/'
+
+    endpoints = [
+        'users/{}'.format(user_id),
+        'todos?userId={}'.format(user_id)
+    ]
+
+    user, tasks = \
+        [requests.get(api + endpoint).json() for endpoint in endpoints]
+
+    username = user.get("username")
+
+    data = {user_id:
+            [{
+                "username": username,
+                "completed": task.get("completed"),
+                "task": task.get("title")
+             } for task in tasks]
+            }
+
+    filename = "{}.json".format(user_id)
+    with open(filename, "w") as file:
+        json.dump(data, file)
