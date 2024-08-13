3fc610c175474e2e980c919565907296a37a6b1f
Ahmed EL Abid <ahmedelabide@gmail.com>
2024-08-13 16:46:27 -0700

    Mandatory

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,7 +6,7 @@ import requests
 def number_of_subscribers(subreddit):
     """Return the count  subscribers on a given subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "linux:0x16.api.advanced"}
+    headers = {"User-Agent": "custom"}
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 404:
         return 0
