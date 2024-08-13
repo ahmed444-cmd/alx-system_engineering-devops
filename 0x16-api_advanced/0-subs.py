3300cea4c9b6d1e20aa4808a0dd44837f99d6588
Ahmed EL Abid <ahmedelabide@gmail.com>
2024-08-13 15:46:46 -0700

    Mandatory

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,10 +8,17 @@ import requests
 
 def number_of_subscribers(subreddit):
     """Returns the count of subscribers for the specified subreddit"""
-    if subreddit is None or type(subreddit) is not str:
+        url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+
+    headers = {'User-Agent': 'RedditDataAnalyzer/1.0 (ALX Africa)'}
+
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    if response.status_code == 200:
+        data = response.json()
+
+        subscribers = data.get('data', {}).get('subscribers', 0)
+
+        return subscribers
+    else:
         return 0
-    r = requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit),
-                     headers={'User-Agent': '0x16-api_advanced:project:\
-v1.0.0 (by /u/firdaus_cartoon_jr)'}).json()
-    subs = r.get("data", {}).get("subscribers", 0)
-    return subs
