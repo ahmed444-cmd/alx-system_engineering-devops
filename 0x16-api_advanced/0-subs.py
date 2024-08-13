51e65a9d2e9998f7b52de7f324cfe753b29444e4
Ahmed EL Abid <ahmedelabide@gmail.com>
2024-08-13 15:51:13 -0700

    Mandatory

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,28 +1,16 @@
 #!/usr/bin/python3
-"""
-Implements the number_of_subscribers function
-"""
-
-import requests
+"""Implements the number_of_subscribers function"""
 
 
 def number_of_subscribers(subreddit):
     """Returns the count of subscribers for the specified subreddit"""
- if subreddit is None or not isinstance(subreddit, str):
+    import requests
+
+    sub_info = requests.get("https://www.reddit.com/r/{}/about.json"
+                            .format(subreddit),
+                            headers={"User-Agent": "My-User-Agent"},
+                            allow_redirects=False)
+    if sub_info.status_code >= 300:
         return 0
 
-    headers = {'User-Agent': 'selBot/1.0'}
-    URL = f'https://www.reddit.com/r/{subreddit}/about.json'
-
-    try:
-        response = requests.get(URL, headers=headers, allow_redirects=False)
-        response.raise_for_status()
-        data = response.json()
-        subscribers = data['data']['subscribers']
-        return subscribers
-
-    except requests.exceptions.RequestException:
-        return 0
-
-    except (KeyError, ValueError):
-        return 0
+    return sub_info.json().get("data").get("subscribers")
