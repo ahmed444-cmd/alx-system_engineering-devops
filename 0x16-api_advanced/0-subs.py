e533071d134c2ac9b55953e0ac5993805f9a66b0
Ahmed EL Abid <ahmedelabide@gmail.com>
2024-08-13 16:36:21 -0700

    Mandatory

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,5 +1,5 @@
-#!/usr/bin/python3
-"""Function to query subscribers on a given Reddit subreddit."""
+#!/usr/bin/pyhton3
+"""This python script query subs on a given Reddit subreddit api url."""
 import requests
 
 
@@ -9,7 +9,7 @@ def number_of_subscribers(subreddit):
     headers = {
         "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
     }
-    response = requests.get(url, headers=headers, allow_redirects=False)
+    response = requests.get(url,headers=headers, allow_redirects=False)
     if response.status_code == 404:
         return 0
     results = response.json().get("data")
